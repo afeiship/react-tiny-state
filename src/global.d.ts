273553ab2b80c953bd91fd5837ce2a4c9d7296f4
afeiship <1290657123@qq.com>
2023-07-14 17:54:24 +0800

    feat: init draft code

---
+++ src/global.d.ts
@@ -0,0 +1,7 @@
+interface NxStatic {
+  $root: any;
+  $get: any;
+  $set: any;
+  $define: any;
+  $call: any;
+}
---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+// https://www.npmjs.com/package/@react-simply/state
+// https://medium.com/simply/state-management-with-react-hooks-and-context-api-at-10-lines-of-code-baf6be8302c
+// https://playcode.io/1533196
+// https://react.dev/reference/react/useReducer
+
+import React, { createContext, useReducer } from 'react';
+import nx from '@jswork/next';
+
+const StateContext = createContext(null);
+
+const StateProvider = ({ reducer, initialState, children }) => {
+  const value = useReducer(reducer, initialState);
+  const [rootState, dispatch] = value;
+
+  nx.$root = rootState;
+  nx.$get = (inPath, inDefaultValue) => nx.get(rootState, inPath, inDefaultValue);
+  nx.$set = (inPath, inValue) => dispatch({ type: '__set__', path: inPath, value: inValue });
+
+  // nx.$root = value[0];
+  // nx.$get = (inKey) = xx;
+  // nx.$set = (inKey, inValue) =xx;
+  // nx.$call = xxx;
+  console.log('value: ', value);
+  return <StateContext.Provider value={value}>{children}</StateContext.Provider>;
+};
+
+// for commonjs es5 require
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = StateProvider;
+}
+
+export default StateProvider;
